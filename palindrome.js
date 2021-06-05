1ea1a7a36d1fc2641ee25258dbfa71730efe0df3
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-06-05 09:44:49 +0700

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -7,3 +7,24 @@ function checkPalindrome(word) {
     }
     return true;
 }
+
+
+//================================ or ========================
+
+function isPalindrome(letters) {
+
+    var characters  = letters.split(''),
+        firstLetter = characters.shift(),
+        lastLetter  = characters.pop();
+
+    if (firstLetter !== lastLetter) {
+        return false;
+    }
+
+    if (characters.length < 2) {
+        return true;
+    }
+
+    return isPalindrome(characters.join(''));
+
+}
